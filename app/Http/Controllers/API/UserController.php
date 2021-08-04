2a72668bc841434343972496b53c63d62b65f5eb
samuel <muel1410@gmail.com>
2021-08-04 09:42:57 +0700

    upload git

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Controllers\API\BaseController;
+use Validator;
+use Auth;
+use Hash;
+
+class UserController extends BaseController
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->responseError('Login failed', 422, $validator->errors());
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            
+            $response = [
+                'token' => $user->createToken('MyToken')->accessToken,
+                'name' => $user->name,
+            ];
+
+            return $this->responseOk($response);
+        } else {
+            return $this->responseError('Wrong email or password', 401);
+        }
+    }
+
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed'],
+            'role_id' => ['required', 'integer'],
+            'school_id' => ['required', 'integer'],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->responseError('Registration failed', 422, $validator->errors());
+        }
+
+        $params = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role_id' => $request->role_id,
+            'school_id' => $request->school_id,
+        ];
+
+        if ($user = User::create($params)) {
+            $token = $user->createToken('MyToken')->accessToken;
+
+            $response = [
+                'token' => $token,
+                'user' => $user,
+            ];
+
+            return $this->responseOk($response);
+        } else {
+            return $this->responseError('Registration failed', 400);
+        }
+    }
+
+    public function profile(Request $request)
+    {
+        return $this->responseOk($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return $this->responseOk(null, 200, 'Logged out successfully.');
+    }
+}
